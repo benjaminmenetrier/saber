4d37637e4d897f665abb751aa669ca0e7b3a99da
Benjamin Menetrier <benjamin.menetrier@met.no>
2026-02-12 15:12:18 +0100

    Improve handling of 2D variables in duplicated mode, and test it

--- src/saber/blocks/SaberCentralBlock.h
+++ src/saber/blocks/SaberCentralBlock.h
@@ -63,6 +63,10 @@ class SaberCentralBlockGroupParameters : public oops::Parameters {
   oops::OptionalParameter<std::vector<SaberOuterBlockParametersWrapper>>
     auxOuterBlocksParams{"auxiliary outer blocks", this};
 
+  // Optional parameter specific to "duplicated"
+  oops::Parameter<std::vector<std::string>>
+    varsOnExtraLevels{"variables on extra levels", {}, this};
+
   // Optional parameters specific to "duplicated and weighted" strategy
   oops::Parameter<double> defOffDiagWeight{"default off-diagonal weight", 0.0, this};
   oops::OptionalParameter<std::vector<OffDiagWeightParameters>>
@@ -214,7 +218,13 @@ class SaberCentralBlock : public util::Printable {
   // Weights for the "duplicated and weighted" strategy
   std::vector<Eigen::MatrixXd> wgtSqrt_;
   // Level for 2D fields (for 3D and 2D fields summation)
-  std::unordered_map<std::string, size_t> lev2d_;
+  std::unordered_map<std::string, int> lev2d_;
+
+
+  // Need for an extra first level for 2D variables
+  std::vector<bool> addExtraFirstLevel_;
+  // Need for an extra last level for 2D variables
+  std::vector<bool> addExtraLastLevel_;
 
   void print(std::ostream &) const {}
 };
